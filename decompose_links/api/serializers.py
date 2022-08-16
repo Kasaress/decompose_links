084978ecca1a1aed763b8d004c36039399713e28
Яна Бубнова <bujhvh@mail.ru>
2022-08-16 12:21:13 +0300

    first iteration

--- decompose_links/api/serializers.py
+++ decompose_links/api/serializers.py
@@ -3,3 +3,31 @@ from rest_framework import serializers
 
 class LinksSerializer(serializers.Serializer):
     link = serializers.CharField(max_length=200)
+
+    def validate_link(self, link):
+        link_as_dick = {}
+        try:
+            link_as_dick['protocol'] = self.initial_data.get(
+                    'link').split('://')[0]
+        except IndexError:
+            link_as_dick['protocol'] = 'null'
+
+        try:
+            link_as_dick['domen'] = self.initial_data.get(
+                'link').split('://')[1].split('/')[0]
+        except IndexError:
+            link_as_dick['domen'] = 'null'
+
+        try:
+            link_as_dick['path'] = self.initial_data.get(
+                'link').split('://')[1].split('/')[1].split('?')[0]
+        except IndexError:
+            link_as_dick['path'] = 'null'
+
+        try:
+            link_as_dick['params'] = self.initial_data.get(
+                'link').split('://')[1].split('/')[1].split('?')[1]
+        except IndexError:
+            link_as_dick['params'] = 'null'
+        link = link_as_dick
+        return link
--- decompose_links/api/views.py
+++ decompose_links/api/views.py
@@ -21,33 +21,8 @@ class LinksView(APIView):
         serializer = LinksSerializer(data=request.data)
         serializer.is_valid()
         if serializer.is_valid():
-            link_as_dick = {}
-            try:
-                link_as_dick['protocol'] = serializer.validated_data.get(
-                    'link').split('://')[0]
-            except IndexError:
-                link_as_dick['protocol'] = 'null'
-
-            try:
-                link_as_dick['domen'] = serializer.validated_data.get(
-                    'link').split('://')[1].split('/')[0]
-            except IndexError:
-                link_as_dick['domen'] = 'null'
-
-            try:
-                link_as_dick['path'] = serializer.validated_data.get(
-                    'link').split('://')[1].split('/')[1].split('?')[0]
-            except IndexError:
-                link_as_dick['path'] = 'null'
-
-            try:
-                link_as_dick['params'] = serializer.validated_data.get(
-                    'link').split('://')[1].split('/')[1].split('?')[1]
-            except IndexError:
-                link_as_dick['params'] = 'null'
-
-            logging.info(link_as_dick)
+            logging.info(serializer.validated_data.get('link'))
             return Response(
-                f'Ссылка залогирована: {link_as_dick}',
+                f"Ссылка в логе: {serializer.validated_data.get('link')}",
                 status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
