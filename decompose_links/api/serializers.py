7f6efa8d973fb052c6bcd6d3993d5ede446e95e3
Яна Бубнова <bujhvh@mail.ru>
2022-08-16 11:40:54 +0300

    все работает, пока без рефакторинга

--- decompose_links/api/serializers.py
+++ decompose_links/api/serializers.py
@@ -3,9 +3,3 @@ from rest_framework import serializers
 
 class LinksSerializer(serializers.Serializer):
     link = serializers.CharField(max_length=200)
-
-    # def validate(self, attrs):
-        
-    #     return super().validate(attrs)
-
-
--- decompose_links/api/views.py
+++ decompose_links/api/views.py
@@ -21,12 +21,31 @@ class LinksView(APIView):
         serializer = LinksSerializer(data=request.data)
         serializer.is_valid()
         if serializer.is_valid():
-            link_as_dick = {
-                'protocol': serializer.validated_data.get('link').split('://')[0],
-                'domen': serializer.validated_data.get('link').split('://')[1].split('/')[0],
-                'path': serializer.validated_data.get('link').split('://')[1].split('/')[1].split('?')[0],
-                'params': serializer.validated_data.get('link').split('://')[1].split('/')[1].split('?')[1]
-            }
+            link_as_dick = {}
+            try:
+                link_as_dick['protocol'] = serializer.validated_data.get(
+                    'link').split('://')[0]
+            except IndexError:
+                link_as_dick['protocol'] = 'null'
+
+            try:
+                link_as_dick['domen'] = serializer.validated_data.get(
+                    'link').split('://')[1].split('/')[0]
+            except IndexError:
+                link_as_dick['domen'] = 'null'
+
+            try:
+                link_as_dick['path'] = serializer.validated_data.get(
+                    'link').split('://')[1].split('/')[1].split('?')[0]
+            except IndexError:
+                link_as_dick['path'] = 'null'
+
+            try:
+                link_as_dick['params'] = serializer.validated_data.get(
+                    'link').split('://')[1].split('/')[1].split('?')[1]
+            except IndexError:
+                link_as_dick['params'] = 'null'
+
             logging.info(link_as_dick)
             return Response(
                 f'Ссылка залогирована: {link_as_dick}',
